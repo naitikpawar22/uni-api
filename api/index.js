17d43876df855b18a631c0e5b5b297d22f4b3f19
Naitik Pawar <73638679+naitikpawar22@users.noreply.github.com>
2025-09-26 19:18:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,6 @@ import { app, connectDB } from '../server.js';
 import serverless from 'serverless-http';
 
 export const handler = async (req, res) => {
-  await connectDB(); // ensure DB is connected for every request
+  await connectDB(); // ensure MongoDB is connected per request
   return serverless(app)(req, res);
 };
